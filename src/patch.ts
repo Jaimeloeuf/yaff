13212653d15097ee17654e57f888248c0a009099
Jaime <jaimeloeuf@gmail.com>
2024-02-12 02:04:04 +0800

    Update docs and names in patch to make it clearer.

--- src/patch.ts
+++ src/patch.ts
@@ -27,17 +27,28 @@ export function patch(originalVNode: VNode, newVNode: VNode) {
     mount(newVNode, element.parentNode!); // @todo Remove the !
   }
 
-  // If new VNode has a string as its child, update textContent if the two strings are different
+  // If the tags are the same, and its child nodes are of the same type,
+  // check if new node has a string as its child, and update the textContent if
+  // the two strings are different.
+  // This is split into 2 separate conditionals so that TS can narrow the type
+  // of vnode child to Arra
   else if (typeof newVNode.child === "string") {
-    if (newVNode.child !== originalVNode.child)
+    if (newVNode.child !== originalVNode.child) {
       element.textContent = newVNode.child;
+    }
   }
 
-  //
+  // This is a no-op conditional used only for TS type narrowing. Because in the
+  // second condition, it already checked for child type equality, if they were
+  // not equal, it would have ran that block of code. So in the third condition
+  // if newVNode's child is a string, the originalVNode's child would definitely
+  // be a string too. However TS can't type narrow it properly, so this helps it
+  // to know that in the final else block, both VNode's child will not be string.
   else if (typeof originalVNode.child === "string") {
   }
 
-  // If VNodes have same the tag, and both have array of VNodes as their child, check for Prop or Child diff
+  // If VNodes have same the tag, and both have array of VNodes as their child,
+  // check for Prop or Child diff
   else {
     // @todo Check the props here
     // @todo I am not going to check the props for now because it would just lengthen the post and miss the point. I might write a third article which contains the full implementation
@@ -49,26 +60,27 @@ export function patch(originalVNode: VNode, newVNode: VNode) {
     // - The new node has more children than the old one
 
     // Find out the lengths
-    // const children1 = VNode1.children; // This only works if the empty else if is included
-    const children1 = originalVNode.child as Array<VNode>;
-    const children2 = newVNode.child;
-    const commonLen = Math.min(children1.length, children2.length);
+    // This only works if the empty no-op else if is included, see it's docs
+    // If not included, `const children1 = originalVNode.child as Array<VNode>;`
+    const originalChild = originalVNode.child;
+    const newChild = newVNode.child;
+    const commonLen = Math.min(originalChild.length, newChild.length);
 
     // Recursively call patch for all the common children
     for (let i = 0; i < commonLen; i++) {
-      patch(children1[i]!, children2[i]!);
+      patch(originalChild[i]!, newChild[i]!);
     }
 
     if (typeof originalVNode.child === "string") {
     }
 
     // If the new node has fewer children
-    if (children1.length > children2.length)
-      children1.slice(children2.length).forEach(unmount);
+    if (originalChild.length > newChild.length)
+      originalChild.slice(newChild.length).forEach(unmount);
     // If the new node has more children
-    else if (children2.length > children1.length)
-      children2
-        .slice(children1.length)
+    else if (newChild.length > originalChild.length)
+      newChild
+        .slice(originalChild.length)
         .forEach((child) => mount(child, element));
   }
 }
