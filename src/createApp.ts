84775cd3765ca4cea3565e8cf2f83268795f73ac
Jaime <jaimeloeuf@gmail.com>
2024-02-11 23:41:46 +0800

    Replace createApp to support rerenders:     There was no easy way to trigger a re-render since calling the patch     function directly isnt the right way to do it as users would then     need to track their own VNodes across changes when it should be the     framework's job.

--- src/createApp.ts
+++ src/createApp.ts
@@ -1,10 +1,29 @@
 import { mount } from "./mount";
+import { patch } from "./patch";
 import type { VNode } from "./VNode";
 
-export function createApp<State>(
-  initialState: State,
-  app: (state: State) => VNode,
-  container: HTMLElement
-) {
-  mount(app(initialState), container);
+export class Yaff<State> {
+  currentVNode: VNode;
+
+  constructor(
+    private readonly container: HTMLElement,
+    private readonly initialState: State,
+    private readonly rootComponent: (state: State) => VNode
+  ) {
+    this.currentVNode = rootComponent.call(this, this.initialState);
+    mount(this.currentVNode, this.container);
+  }
+
+  rerender(newState: State) {
+    const newVNode = this.rootComponent.call(this, newState);
+    patch(this.currentVNode, newVNode);
+    this.currentVNode = newVNode;
+  }
 }
+
+// @todo should use an easier way to type the args by inferring constructor type
+export const yaff = <State>(
+  container: HTMLElement,
+  initialState: State,
+  rootComponent: (state: State) => VNode
+) => new Yaff(container, initialState, rootComponent);
