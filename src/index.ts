2376870771ac9d751a77ab93ede9c48e6c6badae
Jaime <jaimeloeuf@gmail.com>
2024-03-16 22:20:16 +0800

    Fix broken export.

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@
 
 export type * from "./types/index";
 
-export { useState } from "./App";
 export * from "./appBuilder";
 export * from "./f";
 export * from "./plugins/index";
+export { useState } from "./useState";
