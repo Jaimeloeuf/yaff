b0a13b2292b1df9e5ae21270a9c171efc1b2b1a9
Jaime <jaimeloeuf@gmail.com>
2024-04-07 00:55:49 +0800

    Update useState to use current component:     Instead of using a globally shared component hooks array and index,     the use state hook is now tied to individual components.

--- src/useState.ts
+++ src/useState.ts
@@ -1,24 +1,17 @@
+import { getCurrentComponent } from "./renderer";
 import { queueReRender } from "./reRender";
-import type { ComponentHookPair, ComponentHooks } from "./types/index";
-
-const componentHooks: ComponentHooks<any> = [];
-
-/**
- * This needs to be reset to 0 before every single render to ensure that the
- * right component `ComponentHookPair` is returned to the useState caller since
- * this is used to track the useState call order within all the component to
- * determine which pair belongs to which caller.
- */
-let currentHookIndex = 0;
+import type { ComponentHookPair } from "./types/index";
 
 export function useState<T>(initialState: T): ComponentHookPair<T> {
-  let hookPair = componentHooks[currentHookIndex];
+  const currentComponent = getCurrentComponent();
+
+  let hookPair = currentComponent.hooks[currentComponent.hookIndex];
 
   // If hookPair is not undefined it means that this is not the first render as
   // the getter/setter pair already exists. Return it and increment hook index
   // before the next use Hook call.
   if (hookPair !== undefined) {
-    currentHookIndex++;
+    currentComponent.hookIndex++;
     return hookPair;
   }
 
@@ -38,8 +31,8 @@ export function useState<T>(initialState: T): ComponentHookPair<T> {
 
   // Store the getter/setter pair for future renders and increment hook index
   // before the next use Hook call.
-  componentHooks.push(hookPair);
-  currentHookIndex++;
+  currentComponent.hooks.push(hookPair);
+  currentComponent.hookIndex++;
 
   return hookPair;
 }
