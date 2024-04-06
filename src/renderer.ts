ef230ff1cdff8f2f41ee99d6ccaf3c64fb271a43
Jaime <jaimeloeuf@gmail.com>
2024-04-07 00:50:04 +0800

    Create basic renderer that manages render process.

---
+++ src/renderer.ts
@@ -0,0 +1,30 @@
+import { mount } from "./mount";
+import type { Component } from "./types/index";
+
+let currentComponent: Component | null = null;
+
+export function getCurrentComponent() {
+  if (currentComponent === null) {
+    throw new Error(
+      "Internal Error: Tried to get current component before it is set"
+    );
+  }
+
+  return currentComponent;
+}
+
+/**
+ * Render a component
+ */
+export function renderer(
+  component: Component,
+  container: HTMLElement | ParentNode
+) {
+  currentComponent = component;
+
+  currentComponent.hookIndex = 0;
+
+  const vnode = currentComponent.renderFunction();
+
+  mount(vnode, container);
+}
